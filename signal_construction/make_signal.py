a4cde7dec65f0a89d023082e3999cbc49da84299
henry125-student <hc867@scarletmail.rutgers.edu>
2026-04-21 15:00:02 -0400

    saved script generation + new test signal

--- signal_construction/make_signal.py
+++ signal_construction/make_signal.py
@@ -22,6 +22,8 @@
 
 import argparse, math, wave, struct, scipy, numpy as np
 
+# This script has been modified to produce 1 specific type of signal, and thus does not resemble the original script in form.
+
 parser = argparse.ArgumentParser(description='Generate a sine wave.')
 
 parser.add_argument('-s', action='store_true', help='set stereo mode; if missing, the file will be saved in mono', default=False, dest='stereo')
@@ -34,8 +36,8 @@ args = parser.parse_args()
 
 IS_STEREO = True
 SAMPLE_RATE = 48000.0 		# hertz
-NUM_SECONDS = args.duration  	# seconds
-FREQUENCY = args.frequency    	# hertz
+#NUM_SECONDS = args.duration  	# seconds
+#FREQUENCY = args.frequency    	# hertz
 VOLUME = args.volume * 100
 OUTPUT_FILE = args.output 	# filepath
 
@@ -44,26 +46,16 @@ FREQ_GAP = 350
 FREQ_COUNT = 16
 TOTAL_TRIALS = 10
 
-assert NUM_SECONDS > 0.0, 'Duration must be higher than 0 seconds.'
+#assert NUM_SECONDS > 0.0, 'Duration must be higher than 0 seconds.'
 #assert 0 <= FREQUENCY <= 20000.0, 'Wave frequency must be positive and lesser than 20000 Hz.'
 assert 100 <= VOLUME <= 1000.0, 'Volume must be higher than 0 and lesser than 100.'
-
-log = (
-	'Generating a cosine wave.\n\tSample rate: '+str(SAMPLE_RATE)+
-	' Hz\n\tDuration: '+ str(NUM_SECONDS)+
-	' s\n\tFrequency: '+str(FREQUENCY)+ 
-	' Hz\n\tStereo: '+str(IS_STEREO)+
-	'\n\tVolume: '+str(VOLUME)+
-	'\n\tDestination: '+str(OUTPUT_FILE)
-	)
-
-print (log)
 
 file = wave.open(OUTPUT_FILE,'wb')
 file.setnchannels(2 if IS_STEREO else 1)
 file.setsampwidth(2) 
 file.setframerate(SAMPLE_RATE)
 
+#generate chirp
 half_sec_samples = int(0.5 * SAMPLE_RATE)
 samps = np.arange(half_sec_samples) / SAMPLE_RATE
 vals = scipy.signal.chirp(samps, f0=12000, t1=0.5, f1=15000) * VOLUME
@@ -85,27 +77,37 @@ for i in range(int(0.5 * SAMPLE_RATE)):
 for i in range(TOTAL_TRIALS):
 	print(f"Current reps: {i}")
 
+	#1.5 secs Silence
 	for i in range(int(1.5 * SAMPLE_RATE)):
 		data = struct.pack('<hh', 0, 0) if IS_STEREO else struct.pack('<h', 0)
 		file.writeframesraw(data)
 
+	#0.5 secs 12kHz to 15kHz Chirp
 	for i in range(int(0.5 * SAMPLE_RATE)):
 		val = int(vals[i])
 		data = struct.pack('<hh', 0, val) if IS_STEREO else struct.pack('<h', val)
 		file.writeframesraw(data)
 
+	#0.5 secs Silence
 	for i in range(int(0.5 * SAMPLE_RATE)):
-		value = int(VOLUME * math.sin(2 * 440 * math.pi * float(i) / float(SAMPLE_RATE)))
-		data = struct.pack('<hh', 0, value) if IS_STEREO else struct.pack('<h', value)
+		data = struct.pack('<hh', 0, 0) if IS_STEREO else struct.pack('<h', 0)
 		file.writeframesraw(data)
 
-	for i in range(int(NUM_SECONDS * SAMPLE_RATE)):
+	#3 secs 17-23 freq, 
+	for i in range(int(3 * SAMPLE_RATE)):
 		value = 0.0
+
+		#0.2 sec 440Hz beep (1 sec in)
+		if (i >= int(0.8 * SAMPLE_RATE) and i < int(1 * SAMPLE_RATE)):
+			value += int(VOLUME * math.sin(2 * 440 * math.pi * float(i) / float(SAMPLE_RATE)))
+
 		for j in range(FREQ_COUNT):
 			value += VOLUME * math.cos(2 * (17000 + FREQ_GAP*j) * math.pi * float(i) / float(SAMPLE_RATE))
 		data = struct.pack('<hh', 0, int(value)) if IS_STEREO else struct.pack('<h', int(value))
 		file.writeframesraw(data)
-	for i in range(int(0.5 * SAMPLE_RATE)):
+
+	#0.2 secs 660Hz beep
+	for i in range(int(0.2 * SAMPLE_RATE)):
 		value = int(VOLUME * math.sin(2 * 660 * math.pi * float(i) / float(SAMPLE_RATE)))
 		data = struct.pack('<hh', 0, value) if IS_STEREO else struct.pack('<h', value)
 		file.writeframesraw(data)
